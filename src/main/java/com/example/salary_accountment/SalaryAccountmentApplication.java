56d2ffcac5c5c801b56d17dc3711f0a7fc022fca
Ivan <Ivan>
2022-11-16 12:31:27 +0300

    Initial commit

---
+++ src/main/java/com/example/salary_accountment/SalaryAccountmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.salary_accountment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAccountmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryAccountmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salary_accountment/SalaryAccountmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salary_accountment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAccountmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
