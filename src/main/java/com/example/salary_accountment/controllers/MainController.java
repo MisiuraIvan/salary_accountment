5d080bfeef76a9dba8a631760a9b08d6c5a30481
Ivan <Ivan>
2022-11-17 15:54:50 +0300

    Initial commit

--- src/main/java/com/example/salary_accountment/controllers/MainController.java
+++ src/main/java/com/example/salary_accountment/controllers/MainController.java
@@ -1,4 +1,38 @@
 package com.example.salary_accountment.controllers;
 
+import com.example.salary_accountment.models.User;
+import com.example.salary_accountment.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
 public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+/*    @PostMapping
+    public String login(@RequestParam String login, @RequestParam String password, Model model){
+        Optional<User> user = userRepository.findUserByLoginAndPassword(login,password);
+        if(user.isPresent()){
+            Long id=user.get().getUserId();
+            switch(user.get().getPost().getPost()){
+                case "Директор": return "redirect:/admin/"+id.toString();
+                case "Бухгалтер": return "redirect:/accounter/"+id.toString();
+                default: return "redirect:/user/"+id.toString();
+            }
+        }else{
+            return "redirect:/";
+        }
+    }*/
 }
